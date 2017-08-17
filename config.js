64d8ceaebb82fe0f14b4773374ff1df0635a9baf
evuz <jesus_jgb_45@hotmail.com>
2017-08-17 09:45:21 +0200

    get token like as argument

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  token: process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN'
+  token: process.env.TELEGRAM_TOKEN || process.argv[2] || '443455425:AAEu561RTrglItbXeFdFXy9CmMbHXmADVzQ'
 }
