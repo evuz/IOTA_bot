e40ea9056fb922df18a82d3184da8936d795a4e2
Jesus Gómez <jesus_jgb_45@hotmail.com>
2017-12-05 23:36:20 +0100

    feat(info): finish option to give iota price in usd and eur

--- src/bot/functions/info.ts
+++ src/bot/functions/info.ts
@@ -1,6 +1,9 @@
 import * as TelegramBot from 'node-telegram-bot-api';
+import { format } from 'date-fns';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/zip';
+import 'rxjs/add/observable/of';
 
 import { bitfinexService, foreignExchangeService } from './../../services';
 import { ITelegramMessage } from '../../interfaces/TelegramMessage';
@@ -9,9 +12,25 @@ export const info = (telegramBot: TelegramBot, { msg }: ITelegramMessage) => {
   const chatId = msg.chat.id;
 
   try {
-    Observable.zip(bitfinexService.getIOTAPrice(), foreignExchangeService.getRates()).subscribe(([iota]) => {
-      const { price } = iota;
-    });
+    Observable.zip(
+      bitfinexService.getIOTAPrice(),
+      foreignExchangeService.getRates(),
+    )
+      .switchMap(([iota]) => {
+        return Observable.zip(
+          Observable.of(iota),
+          foreignExchangeService.USDtoEUR(iota.price),
+        );
+      })
+      .subscribe(([{ timestamp, price }, eur]) => {
+        const date = format(timestamp * 1000, 'DD/MM/YYYY');
+        const hour = format(timestamp * 1000, 'HH:mm:ss');
+        telegramBot.sendMessage(
+          chatId,
+          `IOTA price is from ${date} to ${hour}:\n` +
+            `${(+price).toFixed(4)}$ = ${eur.toFixed(4)}€`,
+        );
+      });
   } catch (err) {
     throw new Error(err);
   }
