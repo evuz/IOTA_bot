d3d44a2533b92584cc615eda1e3fa668cebc5d08
evuz <jesus_jgb_45@hotmail.com>
2017-12-05 17:43:32 +0100

    feature(cache_service):  create CacheService

---
+++ src/services/cache.service.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/operator/map';
+
+export class CacheService {
+  private expiringTime = 10 * 1000;
+  private expiring = new Map<string, number>();
+  private cacheData = new Map<string, {}>();
+
+  request(url) {
+    if (this.expiring.get(url) > Date.now()) {
+      return Observable.create(obs => obs.next(this.cacheData.get(url)));
+    } else {
+      return Observable.fromPromise(axios.get(url)).map(res => {
+        const data = res.data;
+        this.expiring.set(url, Date.now() + this.expiringTime);
+        this.cacheData.set(url, data);
+        return data;
+      });
+    }
+  }
+}
