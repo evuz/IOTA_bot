1ea78e434639576e7eaf7734d94950739c612dc8
evuz <jesus_jgb_45@hotmail.com>
2017-12-06 20:18:09 +0100

    fix(info): remove useless trycatch

--- src/bot/functions/info.ts
+++ src/bot/functions/info.ts
@@ -11,27 +11,23 @@ import { ITelegramMessage } from '../../interfaces/TelegramMessage';
 export const info = (telegramBot: TelegramBot, { msg }: ITelegramMessage) => {
   const chatId = msg.chat.id;
 
-  try {
-    Observable.zip(
-      bitfinexService.getIOTAPrice(),
-      foreignExchangeService.getRates(),
-    )
-      .switchMap(([iota]) => {
-        return Observable.zip(
-          Observable.of(iota),
-          foreignExchangeService.USDtoEUR(iota.price),
-        );
-      })
-      .subscribe(([{ timestamp, price }, eur]) => {
-        const date = format(timestamp * 1000, 'DD/MM/YYYY');
-        const hour = format(timestamp * 1000, 'HH:mm:ss');
-        telegramBot.sendMessage(
-          chatId,
-          `IOTA price is from ${date} to ${hour}:\n` +
-            `${(+price).toFixed(4)}$ = ${eur.toFixed(4)}€`,
-        );
-      });
-  } catch (err) {
-    throw new Error(err);
-  }
+  Observable.zip(
+    bitfinexService.getIOTAPrice(),
+    foreignExchangeService.getRates()
+  )
+    .switchMap(([iota]) => {
+      return Observable.zip(
+        Observable.of(iota),
+        foreignExchangeService.USDtoEUR(iota.price)
+      );
+    })
+    .subscribe(([{ timestamp, price }, eur]) => {
+      const date = format(timestamp * 1000, 'DD/MM/YYYY');
+      const hour = format(timestamp * 1000, 'HH:mm:ss');
+      telegramBot.sendMessage(
+        chatId,
+        `IOTA price is from ${date} to ${hour}:\n` +
+          `${(+price).toFixed(4)}$ = ${eur.toFixed(4)}€`
+      );
+    });
 };
