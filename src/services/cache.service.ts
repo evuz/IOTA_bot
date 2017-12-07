fbcf64b1a3719d36677db39b48a17a7d85aafb20
evuz <jesus_jgb_45@hotmail.com>
2017-12-07 01:12:02 +0100

    feat(CacheService): improve behaviour when a request is pending

--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -7,17 +7,25 @@ export class CacheService {
   private expiringTime = 10 * 1000;
   private expiring = new Map<string, number>();
   private cacheData = new Map<string, {}>();
+  private requestPending = new Map<string, Observable<any>>();
 
   request(url): Observable<any> {
-    if (this.expiring.get(url) > Date.now()) {
-      return Observable.create(obs => obs.next(this.cacheData.get(url)));
+    if (this.requestPending.get(url)) {
+      return this.requestPending.get(url);
     } else {
-      return Observable.fromPromise(axios.get(url)).map(res => {
-        const data = res.data;
-        this.expiring.set(url, Date.now() + this.expiringTime);
-        this.cacheData.set(url, data);
-        return data;
-      });
+      if (this.expiring.get(url) > Date.now()) {
+        return Observable.create(obs => obs.next(this.cacheData.get(url)));
+      } else {
+        const obs = Observable.fromPromise(axios.get(url)).map(res => {
+          const data = res.data;
+          this.requestPending.delete(url);
+          this.expiring.set(url, Date.now() + this.expiringTime);
+          this.cacheData.set(url, data);
+          return data;
+        });
+        this.requestPending.set(url, obs);
+        return obs;
+      }
     }
   }
 }
