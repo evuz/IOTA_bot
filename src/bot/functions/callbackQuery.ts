b99b88735362a6edacbde403ed06d3bc30998eff
evuz <jesus_jgb_45@hotmail.com>
2017-12-15 09:49:13 +0100

    fix(callback): wait least 200ms when update info

--- src/bot/functions/callbackQuery.ts
+++ src/bot/functions/callbackQuery.ts
@@ -1,17 +1,13 @@
 import { CallbackQuery } from 'node-telegram-bot-api';
 import * as TelegramBot from 'node-telegram-bot-api';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/zip';
+import 'rxjs/add/observable/timer';
 
 import { info } from './info';
 
-export function onCallbackQuery(
-  telegramBot: TelegramBot,
-  callbackQuery: CallbackQuery,
-) {
-  const {
-    message: { chat, message_id },
-    from: { id: userId },
-    id: callback_query_id,
-  } = callbackQuery;
+export function onCallbackQuery(telegramBot: TelegramBot, callbackQuery: CallbackQuery) {
+  const { message: { chat, message_id }, from: { id: userId }, id: callback_query_id } = callbackQuery;
   const [command, value] = callbackQuery.data.split(' ');
 
   const callbackOpts = {
@@ -22,7 +18,7 @@ export function onCallbackQuery(
   switch (command) {
     case 'infoIOTA': {
       telegramBot.editMessageText('Updating...', callbackOpts);
-      info().subscribe(({text, opts}) => {
+      Observable.zip(info(), Observable.timer(200)).subscribe(([{ text, opts }]) => {
         telegramBot.editMessageText(text, Object.assign({}, callbackOpts, opts));
       });
       break;
